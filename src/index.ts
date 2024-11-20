aa39d25fa54b4e05ffc80bfd3ea8fbdf2c9b0319
Hy Nguyen <hynguyen2610@gmail.com>
2024-11-20 11:24:29 +0700

    Add sample index.ts content

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+import express from 'express';
+import bodyParser from 'body-parser';
+import { queueRouter as queueRouter } from './routes/queueRouter';
+
+const app = express();
+const port = 9500;
+
+app.use(bodyParser.json());
+
+app.use('/queue', queueRouter);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Heave Request Server running at http://localhost:${port}`);
+});
